c5cea9166c15a7434c44e690b5cfbc8497276452
eliasyao <eliasyaoyj@gmail.com>
2015-11-30 18:41:14 +0800

    修改 threads and replies

--- e715d1130/src/tw/com/softleader/model/replies.java
+++ e715d1130/src/tw/com/softleader/model/replies.java
@@ -1,5 +1,21 @@
 package tw.com.softleader.model;
 
-public class replies {
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+@Entity
+public class replies {
+	@Id
+	private Integer replyId;
+	private java.util.Date dateCreated;
+	private String title;
+	private String reContent;
+	private Integer rfloor;
+	@ManyToOne
+	@JoinColumn(name = "threadId")
+	private Integer threadId;
+
+	private Integer fPictureId;
 }
--- e715d1130/src/tw/com/softleader/model/threads.java
+++ e715d1130/src/tw/com/softleader/model/threads.java
@@ -1,5 +1,24 @@
 package tw.com.softleader.model;
 
-public class threads {
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+@Entity
+public class threads {
+	@Id
+	private Integer threadId;
+	private String title;
+	private java.util.Date dateCreated;
+	private String thContent;
+	private Integer hit;
+	private Integer replayCount;
+	private java.util.Date dateLastReplied;
+	private char isReadonly;
+	private char topped;
+	@ManyToOne
+	@JoinColumn(name = "boardId")
+	private Integer boardId;
+	private Integer userId;
 }
